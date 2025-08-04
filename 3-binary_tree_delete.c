96cb623c1b1846754acf7ab2ee9778e47d045603
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-04 04:54:35 +0000

    Add file 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete an enitre binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	/* exit case - no children */
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			binary_tree_delete(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			binary_tree_delete(tree->right);
+		}
+	}
+	free(tree);
+}
