3cc4eded4c250d17ddd4a529e06bab5740902268
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-04 08:14:57 +0000

    Update task 6 to remove func call before checking for null

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,9 +11,9 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func) (int))
 {
-	func(tree->n);
-	if (tree != NULL || func != NULL)
+	if (tree != NULL && func != NULL)
 	{
+		func(tree->n);
 		if (tree->left != NULL)
 		{
 			binary_tree_preorder(tree->left, func);
