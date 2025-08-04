cf3c38ec035cf661b141528db9e1147e8042dc28
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-04 04:11:37 +0000

    Update 1-binary_tree_insert_left.c to free memory if parent is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -34,6 +34,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			(new_node->left)->parent = new_node;
 		}
 	}
-
+	else
+	{
+		free(new_node);
+		return (NULL);
+	}
 	return (new_node);
 }
