8ad3ee795d7a14ed173f019e1e4bf71bf9ae52b6
Sheeny Soulsby <roisin.soulsby@gmail.com>
2025-08-05 04:03:11 +0000

    Add files for task 13 - binary tree children nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count the number of nodes with at least one child in a 
+ * binary tree
+ * @tree: pointer to the root node of the tree to measure
+ *
+ * Return: number of nodes with at least one child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (nodes + 1);
+	return (nodes);
+}
